cde0558a5117399b8d062bda776e4b7726535584
Jacob Von Ogden <jvon1197@gmail.com>
2021-11-28 21:46:09 -0500

    finished

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,15 +3,15 @@ import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 import './navbar.css';
 
-// const Menu = () => {
-//     <>
-//     <p> <a href="#home">Home</a> </p>
-//     <p> <a href="#wgpt3">What is GPT3?</a> </p>
-//     <p> <a href="#possibility">Open AI?</a> </p>
-//     <p> <a href="#features">Case Studies</a> </p>
-//     <p> <a href="#blog">Library</a> </p>
-//     </>
-// }
+const Menu = () => (
+    <>
+        <p> <a href="#home">Home</a> </p>
+        <p> <a href="#wgpt3">What is GPT3?</a> </p>
+        <p> <a href="#possibility">Open AI?</a> </p>
+        <p> <a href="#features">Case Studies</a> </p>
+        <p> <a href="#blog">Library</a> </p>
+    </>
+)
 
 // BEM -> Block Element Modifier
 
@@ -25,11 +25,7 @@ const Navbar = () => {
                     <img src={logo} alt="logo"/>
                 </div>
                 <div className="gpt3__navbar-links_container">
-                    <p> <a href="#home">Home</a> </p>
-                    <p> <a href="#wgpt3">What is GPT3?</a> </p>
-                    <p> <a href="#possibility">Open AI?</a> </p>
-                    <p> <a href="#features">Case Studies</a> </p>
-                    <p> <a href="#blog">Library</a> </p>
+                    <Menu />
                 </div>
             </div>
             <div className="gpt3__navbar-sign">
@@ -45,7 +41,11 @@ const Navbar = () => {
                 {toggleMenu && (
                     <div className="gpt3__navbar-menu_container scale-up-center">
                         <div className="gpt3__navbar-menu_container-links">
-
+                            <Menu />
+                            <div className="gpt3__navbar-menu_container-links-sign">
+                                <p>Sign in</p>
+                                <button type="button">Sign up</button>
+                            </div>
                         </div>
                     </div>
                 )}
